c2a34317f90fc44aecb66616c7f60607563f0c99
rohit <rohitweetech@gmail.com>
2024-07-22 10:43:16 +0530

    profile design

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,62 +1,66 @@
 import React from "react";
-import profile from "../assets/image/profile.jpg";
+import profile from "../assets/image/profile.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCamera } from '@fortawesome/free-solid-svg-icons';
+import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
 
 function Profile() {
   return (
-    <>
-      <div>
-        <form className="flex flex-col justify-center items-center h-screen bg-sky-50">
-          <div className="text-center relative">
-            <img
-              className="inline-block h-28 w-28 rounded-full ring-2 ring-white"
-              src={profile}
-              alt="Profile"
-            />
-            
-            <button 
-              className="absolute translate-x-1/2 translate-y-1/2 rounded-full p-2"
-              aria-label="Change Profile Picture"
-            >
-              <FontAwesomeIcon icon={faCamera} className="text-gray-700" />
-            </button>
+    <div className="flex flex-col justify-center items-center min-h-screen bg-sky-50 p-4">
+      <form className="flex flex-col items-center w-full max-w-md">
 
-            <div className="md:w-2/3 mt-8">
-              <input
-                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-96 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-darkblue"
-                id="inline-full-name"
-                type="text"
-                placeholder="Enter Name"
-              />
-            </div>
+      <p className="text-3xl font-semibold">Add Your Business Information</p>
 
-            <div className="md:w-2/3 mt-5">
-              <input
-                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-96 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-darkblue"
-                id="inline-full-title"
-                type="text"
-                placeholder="Enter Title"
-              />
-            </div>
+        <div className="relative mt-10">
+          <img
+            className="inline-block h-32 w-32 rounded-full"
+            src={profile}
+            alt="Profile"
+          />
+          <button 
+            className="absolute bottom-4 right-2 rounded-full p-2"
+            aria-label="Change Profile Picture"
+          >
+            <FontAwesomeIcon icon={faCamera} className="text-darkblue text-xl"/>
+          </button>
+        </div>
 
-            <div className="md:w-2/3 mt-5">
-              <textarea
-                rows="4"
-                cols="50"
-                placeholder="Description"
-                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-96 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-darkblue"
-              ></textarea>
-            </div>
+        <div className="w-full mt-8">
+          <input
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-darkblue"
+            id="inline-full-name"
+            type="text"
+            placeholder="Enter Name"
+          />
+        </div>
 
-            <div className="flex justify-between mt-10">
-              <button className="bg-darkblue text-white font-semibold rounded-full py-2 px-10">Back</button>
-              <button className="bg-darkblue text-white font-semibold rounded-full py-2 px-10">Next</button>
-            </div>
+        <div className="w-full mt-5">
+          <input
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-darkblue"
+            id="inline-full-title"
+            type="text"
+            placeholder="Enter Title"
+          />
+        </div>
+
+        <div className="w-full mt-5">
+          <textarea
+            rows="4"
+            placeholder="Description"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-darkblue"
+          ></textarea>
+        </div>
+
+        <div className="flex sm:flex-row justify-between sm:space-x-4 w-full mt-10">
+          <div className="flex items-center space-x-2">
+            <FaArrowLeft className="text-darkblue text-sm"/>
+            <button className=" text-darkblue font-semibold text-xl">Back</button>
           </div>
-        </form>
-      </div>
-    </>
+          <button className="flex items-center bg-darkblue text-white font-semibold text-lg rounded-full py-1.5 px-8">Next <FaArrowRight className="ml-2 text-sm"/></button>
+        </div>
+      </form>
+    </div>
   );
 }
 
